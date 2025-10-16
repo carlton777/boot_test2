e384be50d4561914d6ffe0e8cc1bce3b0d0200fd
carlton777 <69968419+carlton777@users.noreply.github.com>
2025-10-16 15:34:58 +0900

    Update DemoController.java
    666

--- src/main/java/com/boot/DemoController.java
+++ src/main/java/com/boot/DemoController.java
@@ -14,7 +14,7 @@ public class DemoController {
 	@ResponseBody
 	@RequestMapping("/")
 	public String home() {
-		log.info("Boot Gradle 111 222 333 444~!!!");
+		log.info("Boot Gradle 111 222 333 444 666~!!!");
 		
 		return "gradle";
 //		return "hello";
