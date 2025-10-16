907dea391d99b9acb7c1dbee9228506d91062b29
carlton777 <sangkoo@naver.com>
2025-10-16 15:08:06 +0900

    1

---
+++ src/main/java/com/boot/BootGradle251017Application.java
@@ -0,0 +1,13 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradle251017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradle251017Application.class, args);
+	}
+
+}
