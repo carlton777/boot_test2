69d0bd77be5e668d5e0d5337a24099a1b75e995e
carlton777 <sangkoo@naver.com>
2025-10-16 15:38:49 +0900

    777

--- src/main/java/com/boot/DemoController.java
+++ src/main/java/com/boot/DemoController.java
@@ -14,7 +14,7 @@ public class DemoController {
 	@ResponseBody
 	@RequestMapping("/")
 	public String home() {
-		log.info("Boot Gradle 111 222 333 444 555 666 888~!!!");
+		log.info("Boot Gradle 111 222 333 444 555 666 777 888~!!!");
 		
 		return "gradle";
 //		return "hello";
